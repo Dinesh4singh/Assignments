81f417a7b6d7f2d87f1545189dd6b5f6b4a8eb48
Dinesh4singh <96307252+Dinesh4singh@users.noreply.github.com>
2022-02-06 19:22:38 +0530

    Add files via upload

---
+++ 25 jan-2.js
@@ -0,0 +1,16 @@
+var a= prompt("enter first number");
+var b= prompt("enter second number");
+var c= prompt("enter third number");
+
+if(a>=b && a>=c)
+{
+    alert(a);
+}
+  else if(b>=a && b>=c)
+{
+      alert(b);
+}
+    else
+{
+        alert(c);
+}
